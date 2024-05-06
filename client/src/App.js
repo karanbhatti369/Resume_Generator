43e11ee38ed6132b8db43353a149a05f7ef5d077
karanbhatti369 <karanbhatti369@gmail.com>
2024-05-06 00:05:15 -0400

    Revert "improved css"
    This reverts commit ef9ae3e740e12ea92fbc4e2fa90c235205483333.
    
    # Conflicts:
    #	client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,27 @@
+
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import LoadingScreen from './components/LoadingScreen.js';
 import Home from './components/Home';
 import Resume from './components/Resume';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [result, setResult] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false); // Simulate loading completion
-    }, 3000); // Adjust loading time as needed
-  }, []);
-
-  if (isLoading) {
-    return <LoadingScreen />;
-  }
+  const [result , setResult] = useState({});
 
   return (
     <Router>
-      <div className="App">
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <h1 className="rubik-maps-regular">Resume Builder with OpenAi</h1>
-        <Routes>
-          <Route path='/' element={<Home setResult={setResult} />} />
-          <Route path='/resume' element={<Resume result={result} />} />
-        </Routes>
-      </div>
+    
+    <div className="App">
+    <h1>Resume  Builder with OpenAi</h1>
+    
+  <Routes>
+   <Route path='/' element={<Home setResult={setResult} />} />
+   <Route path='/resume' element={<Resume result={result} />} />
+  </Routes>
+
+
+    </div>
     </Router>
   );
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,4 @@
-import Loading from './LoadingScreen';
+import Loading from './Loading';
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate} from 'react-router-dom';
@@ -74,95 +74,102 @@ const Home = ({setResult}) => {
 
     return (
         <div className="app">
-          <p>Create Job Winning Resume with AI in Seconds</p>
-          <form onSubmit={handleFormSubmit} method='POST' className="resume-form">
-            <div className="form-group">
-              <label htmlFor="fullname">Enter your full name</label>
-              <input
-                name="fullname"
-                type='text'
-                required
-                id="fullname"
-                value={fullName}
-                onChange={(e) => setFullName(e.target.value)} />
-            </div>
-    
-            <div className="form-group">
-              <label htmlFor="currentPosition">Current Position</label>
-              <input
-                name="currentPosition"
-                type='text'
-                required
-                id="currentPosition"
-                value={currentPosition}
-                onChange={(e) => setCurrentPosition(e.target.value)} />
-            </div>
-    
-            <div className="form-group">
-              <label htmlFor="currentLength">For how long?(year)</label>
-              <input
-                name="currentLength"
-                type='number'
-                required
-                id="currentLength"
-                value={currentLength}
-                onChange={(e) => setCurrentLength(e.target.value)} />
-            </div>
-    
-            <div className="form-group">
-              <label htmlFor="currentTechnologies">Technologies used?</label>
-              <input
-                name="currentTechnologies"
-                type='text'
-                required
-                id="currentTechnologies"
-                value={currentTechnologies}
-                onChange={(e) => setCurrentTechnologies(e.target.value)} />
-            </div>
-    
-            <div className="form-group">
-              <label htmlFor="photo">Upload image</label>
-              <input
-                name="photo"
-                type='file'
-                required
-                id="photo"
-                accept='image/x-png,image/jpeg'
-                onChange={(e) => setHeadshot(e.target.files[0])} />
-            </div>
-    
-            {companyInfo.map((company, index) => (
-              <div className="form-group" key={index}>
-                <label htmlFor={`name-${index}`}>Company Name</label>
+            <h1>Resume Builder</h1>
+            <p>Create Job Winning Resume with AI in Seconds</p>
+            <form onSubmit={handleFormSubmit} method='POST'>
+                <label htmlFor="fullname">Enter your full name</label>
                 <input
-                  type='text'
-                  name="name"
-                  required
-                  id={`name-${index}`}
-                  value={company.name}
-                  onChange={(e) => handleUpdateCompany(e, index)} />
-    
-                <label htmlFor={`position-${index}`}>Position Held</label>
+                    name="fullname"
+                    type='text'
+                    required
+                    id="fullname"
+                    value={fullName}
+                    onChange={(e) => setFullName(e.target.value)} />
+
+                <div className='nestedContainer'>
+                    <div>
+                        <label htmlFor="currentPosition">Current Position</label> {/* Corrected to "Current Position" */}
+                        <input
+                            name="currentPosition"
+                            type='text'
+                            required
+                            id="currentPosition"
+                            className='currentInput'
+                            value={currentPosition}
+                            onChange={(e) => setCurrentPosition(e.target.value)} /> {/* Changed to setCurrentPosition */}
+                    </div>
+                    <label htmlFor="currentLength">For how long?(year)</label>
+                    <input
+                        name="currentLength"
+                        type='number'
+                        required
+                        id="currentLength"
+                        className='currentLength'
+                        value={currentLength}
+                        onChange={(e) => setCurrentLength(e.target.value)} /> {/* Changed to setCurrentLength */}
+
+                    <div>
+                        <label htmlFor="currentTechnologies">Technologies used?</label>
+                        <input
+                            name="currentTechnologies"
+                            type='text'
+                            required
+                            id="currentTechnologies"
+                            className='currentTechnologies'
+                            value={currentTechnologies}
+                            onChange={(e) => setCurrentTechnologies(e.target.value)} /> {/* Changed to setCurrentTechnologies */}
+                    </div>
+                </div>
+
+                <label htmlFor="photo">Upload image</label>
                 <input
-                  type='text'
-                  name="position"
-                  required
-                  id={`position-${index}`}
-                  value={company.position}
-                  onChange={(e) => handleUpdateCompany(e, index)} />
-    
-                {companyInfo.length - 1 === index && companyInfo.length < 4 &&
-                  <button type="button" className="add-btn" onClick={handleAddCompany}>Add Company</button>}
-    
-                {companyInfo.length > 1 &&
-                  <button type="button" className="delete-btn" onClick={() => handleRemoveCompany(index)}>Remove</button>}
-              </div>
-            ))}
-    
-            <button type="submit" className="create-resume-btn">Create Resume</button>
-          </form>
+                    name="photo"
+                    type='file'
+                    required
+                    id="photo"
+                    accept='image/x-png,image/jpeg'
+                    onChange={(e) => setHeadshot(e.target.files[0])} />
+
+                {companyInfo.map((company, index) => (
+                    <div className='nestedContainer' key={index}>
+                        <div className='companies'>
+                            <div>
+                                <label htmlFor="name">Company Name</label>
+                                <input
+                                    type='text'
+                                    name="name"
+                                    required
+                                    onChange={(e) => handleUpdateCompany(e, index)}
+                                />
+                            </div>
+                        </div>
+
+                        <div className='companies'>
+                            <div>
+                                <label htmlFor="position">Position Held</label>
+                                <input
+                                    type='text'
+                                    name="position"
+                                    required
+                                    onChange={(e) => handleUpdateCompany(e, index)}
+                                />
+                            </div>
+                        </div>
+
+                        <div className='btn_group'>
+                            {companyInfo.length - 1 === index && companyInfo.length < 4 &&
+                                <button id="addBtn" onClick={handleAddCompany}>Add Company</button>}
+
+                            {companyInfo.length > 1 &&
+                                <button id="deleteBtn" onClick={() => handleRemoveCompany(index)}>Remove</button>}
+                        </div>
+                    </div>
+                ))}
+
+                <button>Create Resume</button>
+            </form>
         </div>
-      );
-    };
-    
-    export default Home;
+    );
+};
+
+export default Home;
